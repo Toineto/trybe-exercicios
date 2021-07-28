e007b71355dd79c80046cbd86f6d55985f02fa55
Toineto <augusto.vilhena4@gmail.com>
2021-07-28 19:15:03 -0300

    adiciona programa 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script3.js
@@ -0,0 +1,16 @@
+let number1 = 30;
+let number2 = 60;
+let number3 = 40;
+
+if (number1 >= number2){
+ console.log(number1);
+}
+else if(number2 >= number3)
+{
+    console.log(number2);
+}
+
+else 
+{
+    console.log(number3);
+}
